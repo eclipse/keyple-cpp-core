62d65afc6cd785a0418d42b860f67274bc0bf1db
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-01-24 17:19:04 +0100

    refactor: simplify build configuration and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.30.0"
     jacoco
 }
+
 buildscript {
     repositories {
         mavenLocal()
@@ -23,11 +24,17 @@ repositories {
     mavenCentral()
     maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.assertj:assertj-core:3.25.3")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 mavenPublishing {
@@ -48,71 +55,6 @@ mavenPublishing {
             license {
                 name.set("Eclipse Public License - v 2.0")
                 url.set("https://www.eclipse.org/legal/epl-2.0/")
-                distribution.set("repo")
-            }
-        }
-        developers {
-            developer {
-                id.set("keyple")
-                name.set("Keyple Contributors")
-                email.set("keyple-dev@eclipse.org")
-            }
-        }
-        scm {
-            connection.set("scm:git:git://github.com/eclipse-keyple/${project.name}.git")
-            developerConnection.set("scm:git:ssh://github.com:eclipse-keyple/${project.name}.git")
-            url.set("https://github.com/eclipse-keyple/${project.name}/tree/main")
-        }
-    }
-}
-
-val javaSourceLevel: String by project
-val javaTargetLevel: String by project
-java {
-    sourceCompatibility = JavaVersion.toVersion(javaSourceLevel)
-    targetCompatibility = JavaVersion.toVersion(javaTargetLevel)
-    println("Compiling Java $sourceCompatibility to Java $targetCompatibility.")
-    withJavadocJar()
-    withSourcesJar()
-}
-
-///////////////////////////////////////////////////////////////////////////////
-//  TASKS CONFIGURATION
-///////////////////////////////////////////////////////////////////////////////
-tasks {
-    spotless {
-        java {
-            target("src/**/*.java")
-            licenseHeaderFile("${project.rootDir}/LICENSE_HEADER")
-            importOrder("java", "javax", "org", "com", "")
-            removeUnusedImports()
-            googleJavaFormat()
-        }
-    }
-    test {
-        testLogging {
-            events("passed", "skipped", "failed")
-        }
-        finalizedBy("jacocoTestReport")
-    }
-    jacocoTestReport {
-        dependsOn("test")
-        reports {
-            xml.required.set(true)
-            csv.required.set(false)
-            html.required.set(true)
-        }
-    }
-    sonarqube {
-        properties {
-            property("sonar.projectKey", "eclipse_" + project.name)
-            property("sonar.organization", "eclipse")
-            property("sonar.host.url", "https://sonarcloud.io")
-            property("sonar.login", System.getenv("SONAR_TOKEN"))
-            System.getenv("BRANCH_NAME")?.let {
-                if (!"main".equals(it)) {
-                    property("sonar.branch.name", it)
-                }
             }
         }
     }
