2ec5131ff7b2a8ca2ac1e66b64d3b5a4040e992e
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-01-26 11:26:42 +0100

    refactor: simplify Javadoc title property usage

--- .github/gradle/javadoc.gradle.kts
+++ .github/gradle/javadoc.gradle.kts
@@ -1,15 +1,17 @@
 tasks {
     withType<Javadoc> {
+        val title = "${project.property("title")} - ${project.version}"
+
         (options as StandardJavadocDocletOptions).apply {
-            windowTitle = project.title + " - " + project.version
+            windowTitle = title
             header = """<div style="margin-top: 7px">
                 <a target="_parent" href="https://keyple.org/">
                     <img src="https://keyple.org/docs/api-reference/java-api/keyple-java-core/1.0.0/images/keyple.png" 
                         height="20px" style="background-color: white; padding: 3px; margin: 0 10px -7px 3px;"/>
                 </a>
-                ${project.title} - ${project.version}
+                $title
             </div>"""
-            docTitle = "${project.title} - ${project.version}"
+            docTitle = title
             setUse(true)
             bottom = System.getenv("Copyright © Eclipse Foundation, Inc. All Rights Reserved.")
             overview = "src/main/javadoc/overview.html"
