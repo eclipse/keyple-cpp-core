d648a237b30c62847a1db6754de8097c65d2e367
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-01-26 11:19:22 +0100

    fix: update Javadoc to use project.title instead of project.name

--- .github/gradle/javadoc.gradle.kts
+++ .github/gradle/javadoc.gradle.kts
@@ -1,15 +1,15 @@
 tasks {
     withType<Javadoc> {
         (options as StandardJavadocDocletOptions).apply {
-            windowTitle = project.name + " - " + project.version
+            windowTitle = project.title + " - " + project.version
             header = """<div style="margin-top: 7px">
                 <a target="_parent" href="https://keyple.org/">
                     <img src="https://keyple.org/docs/api-reference/java-api/keyple-java-core/1.0.0/images/keyple.png" 
                         height="20px" style="background-color: white; padding: 3px; margin: 0 10px -7px 3px;"/>
                 </a>
-                ${project.name} - ${project.version}
+                ${project.title} - ${project.version}
             </div>"""
-            docTitle = "${project.name} - ${project.version}"
+            docTitle = "${project.title} - ${project.version}"
             setUse(true)
             bottom = System.getenv("Copyright © Eclipse Foundation, Inc. All Rights Reserved.")
             overview = "src/main/javadoc/overview.html"
